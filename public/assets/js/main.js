01134aad78d3b1bb27b52beab9f2b03c75d526b0
beatriz-perez <beatriz.pfp@gmail.com>
2020-03-21 11:57:52 +0100

    add favorites delete functionality

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -12,6 +12,7 @@ const urlShowSearchById = 'http://api.tvmaze.com/shows/';
 // RESULTADOS
 const searchResultsIntro = document.getElementById('searchResultsIntro');
 const searchResultsList = document.getElementById('searchResultsList');
+const favListIntro = null;  //************************************************ <--------  OJO por definir
 const favList = document.getElementById('favList');
 
 let searchResultShows = null; // constante preparada para recibir resultados del servidor
@@ -19,15 +20,18 @@ let favShows = []; // constante preparada para recibir elementos añadidos con p
 
 
 // Al cargar la página -----------------------------------------------------------------------------
+// Revisión de info en local storage y aplicación:
+
 const savedFavs = JSON.parse(localStorage.getItem('favShows'));
 if (savedFavs !== null) {
-  for(let fav of savedFavs){
-    addFavourite(fav);
-  }
+  savedFavs.map(fav => addOrRemoveFavourite(fav));
 }
+
 // ACCIONES ****************************************************************************************
 
-function addFavToPannel (newFav) {
+//Gestión de FAVORITOS ------------------------------------
+
+function renderFav(newFav) {
   //LI
   const favCard = document.createElement('li');
   favCard.id = newFav.id + 'FAV';
@@ -45,43 +49,56 @@ function addFavToPannel (newFav) {
   favImage.classList.add('aside__favList--image')
   favImage.src = imageSource;
   favCard.appendChild(favImage);
+  //DELETE BUTTON
+  const favDeleteButton = document.createElement('div');
+  favCard.appendChild(favDeleteButton);
+  const favDeleteButtonContent = document.createTextNode('X');
+  favDeleteButton.appendChild(favDeleteButtonContent);
+  //make X icons react to click
+  favDeleteButton.addEventListener('click', function(){
+    addOrRemoveFavourite(newFav.id.toString());
+    changeCardStyle(newFav.id.toString())
+  });
 };
 
-function getFavInfo (id) {
+function getFavInfo(id) {
   fetch(urlShowSearchById + id)
-  .then(function(response) {
-    if (!response.ok) {throw response;}
-    return response.json();
-  })
-  .then(function(data) {
-    let newFav = data; 
-    addFavToPannel(newFav)
-  })
-  .catch(error => console.log(`Ha sucedido un error: ${error}`));
+    .then(function (response) {
+      if (!response.ok) { throw response; }
+      return response.json();
+    })
+    .then(function (data) {
+      let newFav = data;
+      renderFav(newFav)
+    })
+    .catch(error => console.log(`Ha sucedido un error: ${error}`));
 
 };
-function addFavourite (id) { //Añadimos cada nuevo favorito tanto al listado como a localStorage
+
+function addOrRemoveFavourite(id) { //Añadimos cada nuevo favorito tanto al listado como a localStorage
   if (parseInt(favShows.indexOf(id)) === (-1)) {
     favShows.push(id);
-    localStorage.setItem('favShows', JSON.stringify(favShows));
     getFavInfo(id);
-    } else {
-      console.log('ya está en favoritos')
-    }
+  } else {
+    favShows.splice(parseInt(favShows.indexOf(id)), 1);
+    document.getElementById(id + 'FAV').remove();
+  }
+  localStorage.setItem('favShows', JSON.stringify(favShows));
 };
 
-function changeCardStyle (id) {
-  document.getElementById(id).classList.add('main__favShowStyle');
+function changeCardStyle(id) {
+  document.getElementById(id).classList.toggle('main__favShowStyle');
 }
 
+//Gestión de resultados de BÚSQUEDA ------------------------------------
 
-function renderSearchResultShows (show) {
+function renderSearchResultShows(show) {
   //LI
   const resultCard = document.createElement('li');
   resultCard.id = show.show.id;
   if (parseInt(favShows.indexOf(resultCard.id)) !== (-1)) {
     resultCard.classList.add('main__favShowStyle')
-  } 
+  }
   resultCard.classList.add('main__resultsList--card')
   searchResultsList.appendChild(resultCard);
   //TITLE
@@ -95,35 +112,38 @@ function renderSearchResultShows (show) {
   resultCardImage.src = imageSource;
   resultCard.appendChild(resultCardImage);
   // make card react on clicks
-  resultCard.addEventListener('click', function(){addFavourite(resultCard.id); changeCardStyle(resultCard.id)});
+  resultCard.addEventListener('click', function () { 
+    addOrRemoveFavourite(resultCard.id); 
+    changeCardStyle(resultCard.id) 
+  });
 }
 
-function countResults (count, text) {
+function countResults(count, text) {
   searchResultsIntro.innerHTML = '';
   const searchResultsIntroContent = document.createTextNode(
-    `Estos son los ${count} resultados más populares que contienen "${text}"`
+    `Tenemos ${count} resultados para tu búsqueda "${text}", ordenados por popularidad.`
   );
   searchResultsIntro.appendChild(searchResultsIntroContent);
 }
 
-function showSearchByName () {
-    let query = searchInput.value;
-    fetch(`${urlShowSearchByName}q=${query}`)
-    .then(function(response) {
-      if (!response.ok) {throw response;}
+function showSearchByName() {
+  let query = searchInput.value;
+  fetch(`${urlShowSearchByName}q=${query}`)
+    .then(function (response) {
+      if (!response.ok) { throw response; }
       return response.json();
     })
-    .then(function(data) {
+    .then(function (data) {
       searchResultShows = data;  //******************************** <---- para ver más páginas: acumulador
       countResults(searchResultShows.length, query);  // Ejecutamos el contador 1 vez
-      for (let show of searchResultShows){
+      for (let show of searchResultShows) {
         renderSearchResultShows(show);                // Ejecutamos el renderizador en bucle, por resultado
       }
     })
     .catch(error => console.log(`Ha sucedido un error: ${error}`));
 };
 
-function handleSearchButton (event) {
+function handleSearchButton(event) {
   event.preventDefault();
   searchResultsList.innerHTML = '';
   showSearchByName();
